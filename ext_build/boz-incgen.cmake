6648362b80fbfea62945087d9f6be6818cb70ff2
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2014-03-18 16:12:41 +0100

    Generates versioned headers

---
+++ ext_build/boz-incgen.cmake
@@ -0,0 +1,10 @@
+
+macro (BOZ_INCLUDE_GEN name major minor patch)
+    set(${name}_VERSION ${major}.${minor} CACHE INTERNAL "")
+    set(${name}_SOVERSION ${major}.${minor}.${patch} CACHE INTERNAL "")
+    set(header ${name}.h)
+    message(STATUS "Generates header ${header}, version (${${name}_VERSION})")
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${header} ${CMAKE_BINARY_DIR}/include/${header} @ONLY)
+endmacro()
+
+
