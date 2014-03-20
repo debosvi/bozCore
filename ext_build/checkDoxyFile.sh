bd425118e142c594d0865e645c7ce4605983f24f
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2014-03-20 16:13:28 +0100

    add doxygen check file entry script

---
+++ ext_build/checkDoxyFile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+
+TOP=${PWD}
+
+if [ "$#" -gt 0 ]; then
+    TOP=$1;
+fi
+
+function check_file { 
+    file=$1;
+
+    if [ ! -f ${file} ]; then 
+        echo -e "Not afile: ${file}"; 
+        return
+    fi
+        
+    echo -e "Test file: ${file}"; 
+    hasdoxy=`grep -s "\*.*\file" ${file}`;
+    if [ "${hasdoxy}" == "" ]; then
+        echo -e "\e[0;31m\tNo doxygen '\\\\file' input\e[0m";
+        return
+    fi
+
+    content=`grep -s "\*.*\file" ${file} | awk '{ print $3 }'`;
+    fname=`basename ${file}`;
+    if [ "${content}" != "${fname}" ]; then
+        echo -e "\e[0;31m\tBad doxygen input (${content})\e[0m";
+    else
+        echo -e "\e[0;32m\tOK\e[0m";
+    fi
+}
+
+LIST=`find ${TOP} -type f -name *.[ch]`
+
+if [ ! -d ${TOP} ]; then
+    LIST=${TOP}
+fi
+
+for file in ${LIST}; do 
+    check_file ${file}
+done
