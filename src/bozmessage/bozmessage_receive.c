61d158634911d03c2a4ce7b96a407c5f646ec794
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2015-06-12 13:17:39 +0200

    Fix hash length to compare

--- src/bozmessage/bozmessage_receive.c
+++ src/bozmessage/bozmessage_receive.c
@@ -12,16 +12,15 @@ static int compute_and_check_hash(char const *s, const unsigned int len) {
     char c[PUT_HASH_SIZE];
     md5_update (&hash, s, len);
     md5_final(&hash, c);
-    return byte_diff(s+len, len, c);
+    return byte_diff(s+len, PUT_HASH_SIZE, c);
 }
 
 int bozmessage_receive (bozmessage_receiver_t *b, bozmessage_t *m) {
-    register int r ;
     if (b->maindata.len == b->mainlen) {
         char pack[PUT_HDR_TOTAL_SIZE] ;
         uint16 *magic = (uint16*)&pack[0];
         if (buffer_len(&b->mainb) < PUT_HDR_TOTAL_SIZE) {
-            r = sanitize_read(buffer_fill(&b->mainb)) ;
+            register int r = sanitize_read(buffer_fill(&b->mainb)) ;
             if (r <= 0) return r ;
             if (buffer_len(&b->mainb) < PUT_HDR_TOTAL_SIZE)
                 return (errno = EWOULDBLOCK, 0) ;
@@ -37,6 +36,7 @@ int bozmessage_receive (bozmessage_receiver_t *b, bozmessage_t *m) {
 
     b->mainlen += PUT_HASH_SIZE;
     for (;;) {
+        register int r ;
         register unsigned int n = buffer_len(&b->mainb) ;
         if (n > b->mainlen - b->maindata.len) n = b->mainlen - b->maindata.len ;
         b->maindata.len += buffer_get(&b->mainb, b->maindata.s + b->maindata.len, n) ;
@@ -45,7 +45,7 @@ int bozmessage_receive (bozmessage_receiver_t *b, bozmessage_t *m) {
         if (r <= 0) return r ;
     }
 
-    if((r=compute_and_check_hash(b->maindata.s, b->maindata.len-PUT_HASH_SIZE))%256)
+    if(compute_and_check_hash(b->maindata.s, b->maindata.len-PUT_HASH_SIZE))
         return (errno = EPROTO, -1) ;
 
     m->s = b->maindata.s ;
